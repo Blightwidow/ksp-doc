8915966b4c4caa06910c7ba9e3c41fcf1d0a6d56
Theo Dammaretz <theo@dammaretz.fr>
2022-08-09 11:35:30 +0200

    chore: format all files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const withMarkdoc = require('@markdoc/next.js')
 
-module.exports = withMarkdoc({mode: 'static '})({
+module.exports = withMarkdoc({ mode: 'static ' })({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
 })
